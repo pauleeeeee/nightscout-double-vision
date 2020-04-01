e59c77ff47324224c05be10ed6eb752aa067e675
pauleeeeee <paul.embleton@gmail.com>
2020-04-01 13:38:55 -0600

    updated to fix issues with the Pebble classic

--- src/c/nightscout-double-vision.c
+++ src/c/nightscout-double-vision.c
@@ -823,7 +823,7 @@ static void prv_init(void) {
   //instantiate appmessages
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  app_message_open(128, 128);
 
 }
 
